c41888d35c41fa2b160d34987d5845f6f49a30a0
melo <112213045+0x-melo@users.noreply.github.com>
2022-08-27 00:07:10 +0200

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,393 @@
+from cgi import print_arguments
+import time
+
+
+
+print('''                                                              
+                                                 '1m#hb*^WWkmX+                       !xuvzczXzzvxf?'                          
+                                               :Y#B$B@$$@$@@%bJ1.                    v8B%@B@$@@@@@@Bo_                         
+                                            . .C@@@@a-    i)zZu~..                 ;aBBBB8p];!+|Lh@B@@Y^                       
+                                              XBB$%[        .lfU@BBt`             'h%B$$c.        <q%B@m.                      
+                                             `b8@w`            1m@BBv^           .C@BBMI.           {o@$b                    
+                                           +_J%B8I.            .raZj0J.          (8@Bb^              !k@Bn                     
+                                          ^kWBB@U               <q*kdWi        .^w%@&_               .~W%m,                    
+                                          ~8@B@@r               .Y#*M@t       z {8B$p`                 UBw>I                   
+                                          }%BB$%t               .1WM&Bu       v'UBB@j                  }B8M#'                  
+                                          }B@@@81                [8W#@/       W`v8%B(.                 [$%Ba.                  
+                                          [BB@@$f                f%%&%;       $`vWB@t                  tB$@U                   
+                                          :oB$B@c               `b@&@p       "zUh8B@x                  0B@B/                   
+                                          'U%B@$w;.             (%@&8i        J*oB%@0.                >#@@#"                   
+                                           {%B@%ML;            |&%B%U         ~*8&8Bo"               ,h@@B[                    
+                                           ^q@@@%oLi.        `JBBBBh?          c@@@8@n              ;w8@@)                     
+                                            >oB@%BBac-!;`.`?Q8$B@@8x           `0BB%B%c.        .-]tkBBBJ                  i/v 
+                               .             "hBB$@@@BB@@@BB%&BB@W(             '0BBBBBh[..   ,1m8$%$&BC          '"     ^L%@q.
+                          .:  "n`              fh@@%B@@@$$@B$B@@M[                 f8BB@@@8WM#8@B%8B@h:          .uWl   <oB@J. 
+                           kw: r(               .-UW%B$$@$$@@@&~                     ~Q8B$@B$$@@W%Wz`            .C@?.,C8@BM!  
+                           .|MZLh'                 .?Ub*%@@@M1                          `!tUd#%kj"               I*@ob@@01Bz   
+                             :w8Bx                  `vpkW&az^                            '1v]            ^.      ;M@@@M_"8#'   
+                               1MBz                  .>-~,                                              'k#"    ;0%B@a+'bBj.   
+                                _#@BL^                                                                  'k@n.'tM8&@$%I U@b     
+                                 u%aWBput),                l/.                                          'b@%&B%8w0&@B'(@B-     
+                                 ^bMfv&$@@0`            ,]wu                               ..           ]&@B@M/` [8B&-&@c      
+                                  {%k" O@B@%Q].         0B0                               ]Ww'       lUM%@B@8^   i&B$B@d       
+                                   Y@r +&88B@@8Z|^     j%B<                l1'            Y%B"    v8B%@%h@B@B).  ,&@B@O.       
+                                   `bW>.QWB| "tk@$%Z1+U%@@-               "b%)           .qB@bfLMB@$#c" "a@8$b   !8@$p`        
+                                    ,o&^Y&Bn    `[Co8BB@@@o>              v%@d. .      ']mB@BB@BMu;     ^hBp%@+  ~8BBY         
+                                    .>W8Z#%O       .U@W@@B@%@B%&wvur(?+->U%$BBppko&%B@$B@@@@@@@Y        loBnJ@b 'mBBBv         
+                                      <*$B@o       |88/ ^O@ak##W@$@@B@B@@B@@@@@@@8&aLx]+I0B@&&8B]       IMBrI$@Yh%@@@Z         
+                                       ^b%B8"     f8BY   }8| .   .    ,f%@B8%B%r ..      Z%&@8&%B<      }B@x L$B$Ua@@q         
+                                        'J%@L    t&@U.   ,MC           X8@MM%@B0        !M@QdBBB@#'     ?@@{l@@$z aB@o'        
+                                         .d@%w^ 1&Bf     "*#          !&@a'(&@%B-       1&Bi.Q%@@Bk     [@8b@B@@8;wB@@I        
+                                          fBBB@&@&]       08i        .m@&,  L@B@b      .mBM'  O@BBBY    (@B@@Q.0BuUBB@I        
+                                          lo@&8$@)        n@|        U%@(   ;#$B@v     :a@Q   IaB@B@?   L@Bh!  <&Mp$@@i        
+                                          .bBB@@B@C       nBp       n8$v     tB@BW^    [%@c    !MBB@*_n#B$u     C%&BBBl        
+                                           |%$q  Iq@&|.   <W#'     (8@d.      QB@@U    r%B~     {8B@@@@@B@t     "a@&@B:        
+                                           u8@c    .)*%a? <&@+.   ;M@B~       i*B@%"   m@M'      m%@$@pn$$)      +WB@h'        
+                                          <Qr{{'      "CBBB@Bj   .Z@$t         |%@@U. ;*$U    >Z%@@BB@Qt@8l       1W@d.        
+                                                         }p%@Bdv_O8$U           C%@BI 18@v"/o&$&v:,hB$8p@#'        }WZ.        
+                                                          .1%$B$B@$8Y[^         ,#$@hjm%@%B#L-     ;kBB@@#.         ]          
+                                                           ^*$vQ@$W*%@B@@B%8&MooWBB@BB@@o>          `dBBBb                     
+                                                            XB@B@c    .I~/xvZbbmQZ*B@BBBr            .ZB%t                     
+                                                            [8B@n                  z%B@%l             ^;.                      
+                                                            .Ub/                    Y%$*'                                      
+                                                                                    ^k$k                                       
+                                                                                    IW%B-                                      
+                                                                                     '" . 
+''')
+
+choix=input("Veuillez mettre ce programme en plein ecran pour bien voir son contenu. Appuyez sur la touche 'enter' de votre clavier ")
+if choix == " ":
+
+    choix()
+def choix ():
+    choix=input("Pour les blagues anglaise [1]\nPour les blagues françaises [2]\nReponse: ")
+    if choix == "1":
+        print("That was ass nigga that's why whenever you lose a match of brawlhalla you sneeze in one of yo amazon basics kids storage organizer bins boy you nasty as fuck")
+
+        time.sleep(2.0)
+
+        print("still not funny nigga you gotta turn on your microwave for a stronger wifi connection nigga you garbage")
+
+        time.sleep(2.0)
+
+        print("that was terrible yo barber edge you up with a saints row 4 dubstep gun like shit you dumbass nigga you garbage. ")
+
+        time.sleep(2.0)
+
+        print("you play hopscotch with your grandma")
+
+        time.sleep(2.0)
+
+        print("you was on on Instagram getting lined up by a cricket with a credit card")
+
+        time.sleep(2.0)
+
+        print("you slapped  your brother in the head because he stole ya lollipop you weird as shit ")
+
+        time.sleep(2.0)
+
+        print("you the only fat nigga i know who can do brawlhalla combos with their titties you weird as shit")
+
+        time.sleep(2.0)
+
+        print("your mom got back flipping iguanas drinking apple juice in her living room")
+
+        time.sleep(2.0)
+
+        print("your mom started doing BMX tricks on a laundry bag she weird as shit")
+
+        time.sleep(2.0)
+
+        print("your mom's diabolical she be eating snails and slugs in the bathroom her lil dirty ass ")
+
+        time.sleep(2.0)
+
+        print("your auntie goes to the water park just to throw subway sandwiches at people she weird like shit ")
+
+        time.sleep(2.0)
+
+        print("nigga noo yo barber edge you up with the book of trials and tribulations nigga you bout ugly as shit")
+
+        time.sleep(2.0)
+
+        print("no no no my nigga yo granny was trying to crip walk with a metal plate in her hip like shit lame ass boy")
+
+        time.sleep(2.0)
+
+        print("still not funny you was on the 911 plane dancing to gucci mane ugly ass nigga what you sayin")
+
+        time.sleep(2.0)
+
+        print("boy yo ass gotta slapbox the roaches in yo cabinet just to get your own food you homeless bitch ")
+
+        time.sleep(2.0)
+
+        print("not funny nigga you print 3d figures of yourself and posted a picture with them saying <look at me and the gang!> dumb ass nigga")
+
+        time.sleep(2.0)
+
+        print("you the only fat nigga i know who can do brawlhalla combos with their titties you weird as shit")
+
+        time.sleep(2.0)
+
+        print("nigga tell me why yo fatass uncle named jerry is well known for being a professional frisbee eater nasty ass nigga")
+
+        time.sleep(2.0)
+
+        print("thats why yo mom uppercutted you she said <twinkle twinkle little star> boy yo ass got 3 nipples you ugly ass nigga")
+
+        time.sleep(2.0)
+
+        print("Nope you carry a Lasko Portable Electric 42 inch Tower Fan in yo left pocket just in case yo kidneys collaps from heat failure nigga you dumb")
+
+        time.sleep(2.0)
+
+        print("Uhuh nigga ya mother is a level 10 warrior nigga and she trains zobies to clip her toe nails with safety sizzors boy you ugly as fuck")
+
+        time.sleep(2.0)
+
+        print("Nigga you walked into a chinese resturant and the cashier politely asked you to change yo skin color boy")
+
+        time.sleep(2.0)
+
+        print("nigga yo ass is so stupid you got two pieced by sid the science kid because you forgot newtons laws of physics you ugly ass nigga")
+
+        time.sleep(2.0)
+
+        print("yo momma nipple built like an upside down retarded febreeze bottle bitch nigga yo mom back built like a skittles box ugly ass nigga")
+
+        time.sleep(2.0)
+
+        print("yo ass be whistling through the gap in yo teeth nigga yo teeth be throwing up gang signs repping the tooth fairy ugly ass nigga")
+
+        time.sleep(2.0)
+
+        print("nigga yo ass got sexually assualted by a minecraft spider jockey after yo momma slapped you upside the head with a beanbag")
+
+        time.sleep(2.0)
+
+        print("nigga i caught yo ass picking yo eyelashes off like they was some tulips you smelly ass nigga ill get to cooking on yo anorexic autistic ass")
+
+        time.sleep(2.0)
+
+        print("what she did? she hit you in the back of the head with a expired cherry flavored capri sun")
+
+        time.sleep(2.0)
+
+        print("your grandma was crip walking in the bathroom then she slipped and broke her back ")
+
+        time.sleep(2.0)
+
+        print("yo ass was slapboxing a turtle named romeo cause he was swallowing raccoons for vbucks")
+
+        time.sleep(2.0)
+
+        print("nigga yo ass got sexually assualted by a minecraft spider jockey after yo momma slapped you upside the head with a beanbag")
+
+        time.sleep(2.0)
+
+        print("aye i gotta question, didnt u used to get whoopins with thermometers thats why yo nickname is supa hot fire yo nasty ass")
+
+        time.sleep(2.0)
+
+        print("thats why yo mom uppercutted you she said 'twinkle twinkle little star' boy yo ass got 3 nipples you ugly ass nigga")
+
+        time.sleep(2.0)
+
+        print("nigga you gonna explaain to me why i caught yo ass watching hentai on a gameboy nigga you weird as shit")
+
+        time.sleep(2.0)
+
+        print("laser beam at yo forehead and yo ass started hitting the quan and singing the iranian dung beetle mating call you dumbass nigga")
+
+        time.sleep(2.0)
+
+        print("boy i caught yo ass tryna smoke a pack wit yogi bear that nigga smacked you upside the head wit a flinstone gummi and yo ass said 'im the coochie man'")
+
+        time.sleep(2.0)
+
+        print("nigga i caught yo ass selling rotten flesh from minecraft on the dark web you nasty ass nigga im on yo chrome nigga you built like a chinese acorn LOL")
+
+        time.sleep(2.0)
+
+        print("nigga explain to me how i caught yo ass in the elementary school playground trying to T.P ant hills you bummy weird ass nigga")
+
+        time.sleep(2.0)
+
+        print("yo ass is known for putting a silencer on a potato launcher and shooting up your local halloween store you fatass nigga thats why yo ugly ass grandmother tries tackling the statue of liberty for fun")
+
+        time.sleep(2.0)
+
+        print("nigga i caught u slapboxing a nigga name donitello for 20 vbucks like shit nigga you was a known school threat because you brought a baguette to school for bring your pet day smelly nigga")
+
+        time.sleep(2.0)
+
+        print("yo ass play with an oujia board and thought yo ass was summoning demons")
+
+        time.sleep(2.0)
+
+        print("nigga tell me why yo grandma got poison dart frogs mating in the back of her microwave that nasty bitch said it adds a little bit of seasoning")
+
+        print("finish.")
+
+        time.sleep(500.0)
+
+
+    if choix == "2":
+        print("Je t'ai traité d'idiot dans un vc discord et ta grand-mère m'a envoyé un texto me demandant si je voulais rencontrer Dieu")
+
+        time.sleep(2.0)
+
+        print("Tu as boxé des ratons laveurs pour le dernier Dunkin Donuts spécial 2 pour 2, abruti de négro.")
+
+        time.sleep(2.0)
+
+        print("T'as pris l'habitude de violer des meufs dans la rue car tu crois que ça te donne de l'xp, mongole va")
+
+        time.sleep(2.0)
+
+        print("Un jour j'ai vu ta grosse daronne le zombie s'accouplée avec un creeper, paysan de merde")
+
+        time.sleep(2.0)
+
+        print("Quand tu es sur discord tu fais plus de bruit qu'un dragon qui crache des boules de feu dans minecraft, sombre asmathique")
+
+        time.sleep(2.0)
+
+        print("PizzaHut a mis ta maison sur liste noire parce que ta grand-mère faisait du yoga dans le salon, le livreur était traumatisé. ")
+
+        time.sleep(2.0)
+
+        print("ta gueule, t'es le genre de mec qui perd un ami d'enfance dans une bataille de boules de neige")
+
+        time.sleep(2.0)
+
+        print("ta grand-mere la pute a perdu 3 heures de sa vie à essayer de communiquer avec Chewbacca. ")
+
+        time.sleep(2.0)
+
+        print("fils de pute tu m'explique pourquoi ta daronne elle a commencer a prie batman comme sa")
+
+        time.sleep(2.0)
+
+        print("ton grand-père n'a pas pu revenir de la seconde guerre mondiale parce qu'il était trop occupé à beatboxer des soldats japonais")
+
+        time.sleep(2.0)
+
+        print("Ta mère a rassemblé une église pour invoquer le Père Noël elle est trop bizzard")
+
+        time.sleep(2.0)
+
+        print("ta mère s'est fait taper derrière la tête avec une peau de banane elle est trop bizzare")
+
+        time.sleep(2.0)
+
+        print("tu t'es curé le nez comme une mine d'or pour trouver de l'argent supplémentaire")
+
+        time.sleep(2.0)
+
+        print("tu as ouvert des biscuits de la chance pour que Dieu te donne une ps3 sale merde")
+
+        time.sleep(2.0)
+
+        print("ta mere est connu pour avoir ete 2em de la loud list ")
+
+        time.sleep(2.0)
+
+        print("ton pere comme sa il est venu sur the mimic il a commencer a crie mic up mic up il est trop bizzare")
+
+        time.sleep(2.0)
+
+        print("fils de pute tu m'explique pourquoi j'ai trouver une table d'enchant dans ta chambre ?")
+
+        time.sleep(2.0)
+
+        print("pourquoi tu tires de boules de feu de tes rotules negro")
+
+        time.sleep(2.0)
+
+        print("nan mon gars tu as fait un clone de toi-même en utilisant une mini imprimante 3D ")
+
+        time.sleep(2.0)
+
+        print("Dis-moi pourquoi j'ai surpris ton grand-père en train de donner un coup de tête professionnel à toutes les boîtes de thon")
+
+        time.sleep(2.0)
+
+        print("tu m'explique pourquoi j'ai vu ta grosse daronne dans travailler dans un entreprise de crayon de couleur")
+
+        time.sleep(2.0)
+
+        print("tu t'es fait gifler par un hippopotame pour avoir fait un bain de minuit dans sa flaque de boue")
+
+        time.sleep(2.0)
+
+        print("ta grand-mère est un agent dormant russe, j'ai dit Golden Apple K500 et elle a commencé à faire des mouvements de karaté.")
+
+        time.sleep(2.0)
+
+        print("mon garçon, non, tu es le meilleur taille-crayon du monde, tu tailles les crayons avec l'espace entre tes dents. ")
+
+        time.sleep(2.0)
+
+        print("ton pere a commencer a mettre dans tchat <qui stam 72h> il est vraiment trop bizzard")
+
+        time.sleep(2.0)
+
+        print("ta gueule ta commencer a tirer des boules de feu de tes rotules")
+
+        time.sleep(2.0)
+
+        print("Michael Jackson est revenu à la vie pour gifler ta daronne avec un cure-dent")
+
+        time.sleep(2.0)
+
+        print("j'ai lancer une bombe boogie woogie sur ta daronne comme sa elle a commencer a twerker sur la table")
+
+        time.sleep(2.0)
+
+        print("Ta mère a fouillé dans ton téléphone et tu avais les Tortues Ninja en numérotation rapide.")
+
+        time.sleep(2.0)
+
+        print("Ta grand-mère t'a envoyé un lien grabify elle est vraiment trop bizzare")
+
+        time.sleep(2.0)
+
+        print("ta gueule ta la voix d'un scammeur fortnite")
+
+        time.sleep(2.0)
+
+        print("tu comprend ton pere a mit le tag aigis il est trop bizzare")
+
+        time.sleep(2.0)
+
+        print("j'ai vu ta grosse daronne elle a commencer a faire un spawner a mob chez elle est esat trop bizzare")
+
+        time.sleep(2.0)
+
+        print("Tu as une chaise de bureau ajustable rembourrée de cuir dans ta baignoire, juste au cas où tes rotules seraient fatiguées quand tu prends une douche")
+
+        time.sleep(2.0)
+
+        print("Tes sourcils on dirait des lingots de diamant dans minecraft, paysan ignoble")
+
+        time.sleep(2.0)
+
+        print("Tu t'es mis le doigt dans le cul pour essayer de trouver du diamant, sale pauvre")
+
+        time.sleep(2.0)
+
+        print("Ta mère le sanglier elle m'a foncé dessus pour des pommes d'or dans minecraft elle est totalement fauchée")
+
+        time.sleep(2.0)
+
+        input("")
+        
+    elif choix != "3":
+        print("internal server error. Veuillez relancer ce programe et mettre soit '1' ou '2'.")
+        time.sleep(500.0)
+
+choix()
+
+        
